6ec0137a34bf208061e143cfc03f3d26583c52ea
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-11-10 22:17:44 +0900

    PREFECTURES_ROMAN削除

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -5,6 +5,6 @@ export { PUBLISH_ERROR_CODE } from "./errorCode";
 export type { PublishErrorCode } from "./errorCode";
 export { FIRESTORE_COLLECTION_PATH } from "./firestore";
 export type { FirestoreCollectionPath } from "./firestore";
-export { PREFECTURES_JA, PREFECTURES_ROMAN } from "./province";
+export { PREFECTURES_JA } from "./province";
 export { STOP_CONDITION_LABELS } from "./stopCondition";
 export { STUB_DB } from "./stubDB";
